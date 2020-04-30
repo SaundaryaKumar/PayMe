0d40ee670ead64c81ae1aff62f4e0a5a98ff1316
unknown <sksaundarya10@gmail.com>
2020-04-30 21:57:00 +0530

    initial commit

---
+++ app/src/main/java/com/dromeus/delta/payme/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.dromeus.delta.payme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+            }, 1000);
+
+        }
+    }
+
